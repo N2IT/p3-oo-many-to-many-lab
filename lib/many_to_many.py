d5c2c3ba13a9f836693b421f371dc5bbee5c4810
Tony Eder <tonyeder11@gmail.com>
2024-02-19 08:45:04 -0800

    35% completed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,27 @@
 class Author:
-    pass
+    
+    def __init__(self,name):
+        self.name = name
 
 
 class Book:
-    pass
+    
+    def __init__(self,title):
+        self.title = title
 
 
 class Contract:
-    pass
+    
+    def __init__(self, author, book, date, royalties):
+        # validate that type author == author
+        if isinstance(author, Author) and isinstance(book, Book) and isinstance(self.date, str):
+            self.author = author
+            self.book = book
+            self.date = date
+        else:
+            raise Exception
+        
+        
+        self.royalties = royalties
+
+    
