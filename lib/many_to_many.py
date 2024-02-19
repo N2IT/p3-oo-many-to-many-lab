215de315229c2f1bdb466670efd807b534f7a6b9
Tony Eder <tonyeder11@gmail.com>
2024-02-19 09:28:15 -0800

    78% complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,27 +1,52 @@
 class Author:
-    
+
+    all = []
+
     def __init__(self,name):
         self.name = name
 
+    def add_contracts(self, book):
+        Contract(self, book)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self ]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
 
 class Book:
+
+    all = []
     
     def __init__(self,title):
         self.title = title
 
+    def contracts(self):
+        return [ contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+
+    def add_author(self, author):  
+        Contract(author, self)
 
 class Contract:
+
+    all = []
     
     def __init__(self, author, book, date, royalties):
-        # validate that type author == author
-        if isinstance(author, Author) and isinstance(book, Book) and isinstance(self.date, str):
+        if isinstance(author, Author) and isinstance(book, Book) and isinstance(date, str) and isinstance(royalties, int):
             self.author = author
             self.book = book
             self.date = date
+            self.royalties = royalties
+            Contract.all.append(self)
         else:
             raise Exception
+
+
         
         
-        self.royalties = royalties
+        
 
     
